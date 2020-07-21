96577e33cf5aa93c44f0ec561f4e1ec1e788d9d6
alexandr <alexandr@debtop.alexandr>
2020-07-21 14:24:22 +0700

    Make file added to each project

---
+++ School/Dz7_MakeFiles/izi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "izi.h"
+
+int main(void)
+{
+  printf("HelloWorld!\n");
+  printf("(O_O)\n");
+  printf("(^_^)\n");
+  printf("(*_*)\n");
+  return 0;
+}
